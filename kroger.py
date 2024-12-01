e4035860e8894984d994173b3455aaaa8235125e
carlbloom <cbloom11@pride.hofstra.edu>
2024-12-01 14:01:38 -0500

    updated home page to show random selection of items instead of predetermined set

--- kroger.py
+++ kroger.py
@@ -1,5 +1,6 @@
 
 import requests
+import random
 
 def get_basic_auth_token():
     return "Y29ybnVjb3BpYTItMjQzMjYxMjQzMDM0MjQ0ZTQ2NmI0MTM2MzY2OTRiNTUyZTU5NmEzMDMyMzA2MjQ3NTU1OTZiNmM3NTUwNTY2Yjc2NjgyZTU0MzA2NTYyNjE0ZjY4NjU0NDRlNjMzNDY3NGY0NDUzNDQ3NDM2NzEzNjRkNzQ0YjZkMTMzNzM0MzU5NTUwMjk5MTcyOTprOUE2ZVo2Y0dhemp6RkhLNkNjNnNNSU4wbHJWMzdjMHZIb3l5M0Q4"
@@ -13,7 +14,12 @@ def get_auth_token():
     return x.json()["access_token"]
 
 def get_hot_products(auth_token):
-    filter = {"filter.term": "Kroger", "filter.limit": "5", "filter.locationId": "01100002"}
+    #randomly select filter term to display
+    filterlist = ["Kroger","Milk","Chicken","Cheese","Bread","Beef","Soda","Fruit","Vegetable"]
+    num = random.randint(1, len(filterlist))
+    filterterm = filterlist[num-1]
+    #set filter and pass info on to get products
+    filter = {"filter.term": filterterm, "filter.limit": "5", "filter.locationId": "01100002"}
     return get_products(filter, auth_token)
     
 def search_product(term, auth_token):  #todo change location later
